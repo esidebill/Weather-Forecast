0e1c63347ee1634384d799150024688f2ade9865
Bill <billwalsh006@gmail.com>
2017-08-14 17:31:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,11 +17,44 @@ $(document).ready(function() {
         $(".location").html(
           "Your location: " + data.sys.country + ", " + data.name
         );
-        $(".conditions").html("Weather conditions: " + data.weather[0].main);
+        $(".conditions").html("Currently: " + data.weather[0].main);
         $("#F").html("Temperature: " + Math.round(tempF) + "°");
         $("#C").html("Temperature: " + Math.round(tempC) + "°");
         $(".humidity").html("Humidity: " + data.main.humidity + "%");
         $(".icon").attr("src", data.weather[0].icon);
+
+        //select case to determine background image
+        switch (data.weather[0].main) {
+          case "Clear":
+            $("body").css(
+              "background-image",
+              "url('https://images.unsplash.com/photo-1464495310703-83aac94583ee?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=95f84e8f19e07b781bd60190e8a72dac')"
+            );
+            break;
+          case "Rain":
+            $("body").css(
+              "background-image",
+              "url('https://images.unsplash.com/19/drops.JPG?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=59ee0e464d82b20a03f0792d58f07c05')"
+            );
+            break;
+          case "Clouds":
+            $("body").css(
+              "background-image",
+              "url('https://images.unsplash.com/photo-1481433705997-f3c061a4eac8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=59f5add1c8d7e3f23c7a0397c9c97bf2')"
+            );
+            break;
+          case "Snow":
+            $("body").css(
+              "background-image",
+              "url('https://images.unsplash.com/photo-1489515217757-5fd1be406fef?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=d87a3c29e139477c450c787a63bdce6f')"
+            );
+            break;
+          default:
+            $("body").css(
+              "background-image",
+              "url('https://images.unsplash.com/photo-1460602692976-8eab38c11f9d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=6ac548b7711f39cdde0bad17f6b60ef8')"
+            );
+        }
 
         //button that changes from F to C
         $("#fc").on("click", function() {
@@ -31,4 +64,3 @@ $(document).ready(function() {
     });
   }
 });
-
